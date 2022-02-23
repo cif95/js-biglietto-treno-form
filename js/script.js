0843e5d2f7689146d3cf1966ca6adb6795267513
cif95 <vivianacifarelli9@gmail.com>
2022-02-24 00:06:52 +0100

    corrections

--- js/script.js
+++ js/script.js
@@ -7,13 +7,12 @@ if  (!isNaN(userName.value)){
 	console.log('l\'utente ha inserito un valore valido');
 }
 
-let travelDistance = document.getElementById('travel-distance-input');
-console.log(travelDistance.value + ' : km da percorrere inseriti dall\'utente');
-
-if ((isNaN(travelDistance.value) || (travelDistance.value > 1000) || (travelDistance.value < 10))){
+let travelDistance = parseFloat(document.getElementById('travel-distance-input').value);
+console.log(travelDistance + ' : km da percorrere inseriti dall\'utente');
+if ((isNaN(travelDistance) || (travelDistance > 1000) || (travelDistance < 10))){
 	console.log('Non hai inserito un valore valido, ne creo uno casuale');
-	travelDistance.value = Math.floor(Math.random()*1000) + 10;
-	console.log('il numero di km generato casualmente è ' + travelDistance.value)
+	travelDistance = Math.floor(Math.random()*1000) + 10;
+	console.log('il numero di km generato casualmente è ' + travelDistance)
 } else {
 	console.log('l\'utente ha inserito un numero valido');
 }
@@ -27,7 +26,7 @@ document.getElementById('user-age-range').addEventListener('change',
 });
 
 
-const basePrice = travelDistance.value * 0.27;
+const basePrice = travelDistance * 0.27;
 console.log('prezzo base del biglietto : € ' + basePrice);
 
 
@@ -48,15 +47,20 @@ function(){
 }
 console.log(finalPrice);
 document.getElementById('ticket').classList.remove('d-none');
+travelDistance = document.getElementById('travel-distance-input');
 document.querySelector('p#ticket-price').innerHTML = finalPrice.toFixed(2) + '€';
 document.querySelector('section#ticket div.name p').innerHTML = userName.value;
+let message =  `Ciao ${userName.value}, 
+il prezzo del biglietto è ${finalPrice.toFixed(2)}
+per un tragitto di ${travelDistance.value}km.`
+document.getElementById('message').innerHTML = message;
 })
 
 
-
-let UndoBtn = document.getElementById('btn-undo');
-UndoBtn.addEventListener('click', 
+let undoBtn = document.getElementById('btn-undo');
+undoBtn.addEventListener('click', 
 function(){
 	document.getElementById('ticket').classList.add('d-none');
 	document.getElementById('user-form').reset();
+	
 })
