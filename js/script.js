4ebd74d67df5a69605141c2e307beb30d64cedfb
cif95 <vivianacifarelli9@gmail.com>
2022-02-24 15:08:08 +0100

    correction

--- js/script.js
+++ js/script.js
@@ -1,60 +1,37 @@
 
 let userName = document.getElementById('user-name-input');
-console.log(userName.value + ' : nome e cognome dell\'utente');
-if  (!isNaN(userName.value)){
-	console.log('Non hai inserito un valore valido');
-} else {
-	console.log('l\'utente ha inserito un valore valido');
-}
-
-let travelDistance = parseFloat(document.getElementById('travel-distance-input').value);
-console.log(travelDistance + ' : km da percorrere inseriti dall\'utente');
-if ((isNaN(travelDistance) || (travelDistance > 1000) || (travelDistance < 10))){
-	console.log('Non hai inserito un valore valido, ne creo uno casuale');
-	travelDistance = Math.floor(Math.random()*1000) + 10;
-	console.log('il numero di km generato casualmente è ' + travelDistance)
-} else {
-	console.log('l\'utente ha inserito un numero valido');
-}
-
-let userAge = "";
-
-document.getElementById('user-age-range').addEventListener('change', 
-	function() {
-		userAge = this.value;
-		console.log('fascia d\età dell\'utente : ' + userAge);
-});
-
-
-const basePrice = travelDistance * 0.27;
-console.log('prezzo base del biglietto : € ' + basePrice);
-
-
-let finalPrice = basePrice;
+let travelDistance = document.getElementById('travel-distance-input');
+let userAge = document.getElementById('user-age-range');
 
 
 let createTicketBtn = document.getElementById('btn-create');
 createTicketBtn.addEventListener('click', 
 function(){
-	if (userAge === "Minorenne") {
+	const basePrice = travelDistance.value * 0.27;
+	console.log('prezzo base del biglietto : € ' + basePrice);
+	let finalPrice = basePrice;
+	if (userAge.value === "Minorenne") {
 		finalPrice *= 0.83;
 		console.log('l\'utente è minorenne, pertanto ha diritto ad uno sconto del 17% ed il prezzo scontato è € ' + finalPrice);
-	}	else if ( userAge === "Over 65") {
+	}	else if ( userAge.value === "Over 65") {
 		finalPrice *= 0.67;
 		console.log('l\'utente è over 65, pertanto ha diritto ad uno sconto del 33% ed il prezzo scontato è € ' + finalPrice);
-	} else if (userAge === "Maggiorenne") {
+	} else if (userAge.value === "Maggiorenne") {
 			console.log('l\'utente non ha diritto ad uno sconto');
-}
-console.log(finalPrice);
-document.getElementById('ticket').classList.remove('d-none');
-travelDistance = document.getElementById('travel-distance-input');
-document.querySelector('p#ticket-price').innerHTML = finalPrice.toFixed(2) + '€';
-document.querySelector('section#ticket div.name p').innerHTML = userName.value;
-let message =  `Ciao ${userName.value}, 
-il prezzo del biglietto è ${finalPrice.toFixed(2)}
-per un tragitto di ${travelDistance.value}km.`
-document.getElementById('message').innerHTML = message;
+	} else {
+		console.log('l\'utente non ha selezionato una fascia d\'età')
+	}
+
+	document.getElementById('ticket').classList.remove('d-none');
+	travelDistance = document.getElementById('travel-distance-input');
+	document.querySelector('p#ticket-price').innerHTML = finalPrice.toFixed(2) + '€';
+	document.querySelector('section#ticket div.name p').innerHTML = userName.value;
+	let message =  `Ciao ${userName.value}, 
+	il prezzo del biglietto è di € ${finalPrice.toFixed(2)}
+	per un tragitto di ${travelDistance.value}km.`
+	document.getElementById('message').innerHTML = message;
 })
+
 
 
 let undoBtn = document.getElementById('btn-undo');
@@ -62,5 +39,6 @@ undoBtn.addEventListener('click',
 function(){
 	document.getElementById('ticket').classList.add('d-none');
 	document.getElementById('user-form').reset();
-	
+	document.querySelector('p#ticket-price').innerHTML = "";	
+	message= "";
 })
